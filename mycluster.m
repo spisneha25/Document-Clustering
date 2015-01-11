792ad14690211961afccba294a2df425c4ae6d43
Sneha <spisneha25@users.noreply.github.com>
2015-01-10 22:22:45 -0400

    Update mycluster.m

--- mycluster.m
+++ mycluster.m
@@ -1,21 +1,4 @@
 function [ class ] = mycluster( D, k )
-    %
-    % Your goal of this assignment is implementing your own text clustering algo.
-    %
-    % Input:
-    %     bow: data set. Bag of words representation of text document as
-    %     described in the assignment.
-    %
-    %     K: the number of desired topics/clusters. 
-    %
-    % Output:
-    %     class: the assignment of each topic. The
-    %     assignment should be 1, 2, 3, etc. 
-    %
-    % For submission, you need to code your own implementation without using
-    % any existing libraries
-
-    % YOUR IMPLEMENTATION SHOULD START HERE!
 
     %%%Initialize the EM Algorithm parameters%%%
     threshold = 1/100000;
